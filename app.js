fd8ba504f1cb2037a60100811401b4f29498a20a
Makoto Inomata <inomar1486@gmail.com>
2020-08-12 20:34:14 +0900

    fix: set_emoji_status api

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const setProfile = require('./src/setProfile');
 const getTasks = require('./src/getTasks');
-const status = require('./src/status');
+const status = require('./src/status'); 
 
 
-getTasks().then(({ data }) => {
-  setProfile(status.currentEmojiStatus(data.length)).then(d => {
-    if (!d.data.ok) console.log(d.data.error);
+app.get('/set_emoji_status', (req, res) => {
+  getTasks().then(({ data }) => {
+    setProfile(status.currentEmojiStatus(data.length)).then(d => {
+      if (!d.data.ok) {
+        console.log(d.data.error);
+      }
+    }).catch(error => {
+      console.log(error.message);
+    });
   }).catch(error => {
-    console.log(error.message);
-  });
-}).catch(error => {
-  setProfile(status.errorStatus(error.message));
+    setProfile(status.errorStatus(error.message));
+  })
+  res.json({ message: 'set status' });
+});
+
+app.listen(port, () => {
+  console.log(`emoji status app listening at http://localhost:${port}`);
 });
